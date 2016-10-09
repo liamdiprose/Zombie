82097aafc2f3d60dd84883f5f630470808797099
BlankSparrow <blanksparrow@outlook.com>
2016-10-09 16:23:41 +1300

    now working map walking sim

--- game/player.c
+++ game/player.c
@@ -1,16 +1,12 @@
 #include "system.h"
 #include "navswitch.h"
 #include "pio.h"
-<<<<<<< HEAD
 #include "level.h"
-=======
->>>>>>> 80b432be3a54b6755150eed4a48d6ce5090959ac
 #include "player.h"
 #include "point.h"
 #include "pacer.h"
 
 void player_init(player players[]){
-<<<<<<< HEAD
      players[0].position.x = 2;
      players[0].position.y = 2;
 
@@ -60,47 +56,4 @@ void player_update(void *data)
     uint32_t player_get_beat_rate(player players[]){
         return players[0].health; 
     }
-=======
-     players[0].position.x = 3;
-     players[0].position.y = 3;
-     navswitch_init ();
-}
-
-void player_update(player *players)
-{
-    static bool init = 0;
-
-    if (!init)
-    {
-        navswitch_init ();
-        init = 1;
-    }
-
-    navswitch_update ();
-
-    if (navswitch_push_event_p (NAVSWITCH_SOUTH) == true)
-    {   
-        players[0].position.y += 1;
-    }
-
-    if (navswitch_push_event_p (NAVSWITCH_EAST) == true)
-    {   
-        players[0].position.x += 1;
-    }
-    
-    if (navswitch_push_event_p (NAVSWITCH_NORTH))
-    {
-        players[0].position.y -= 1;
-    }
-    
-    if (navswitch_push_event_p (NAVSWITCH_WEST))
-    {   
-        players[0].position.x -= 1;
-    }
-    
->>>>>>> 80b432be3a54b6755150eed4a48d6ce5090959ac
-    /*if (navswitch_push_event_p (NAVSWITCH_PUSH))
-    {   
-        players[0].position.x = 0;
-    }*/
 }
--- game/player.h
+++ game/player.h
@@ -7,19 +7,11 @@
 
 typedef struct {
    point position;
-<<<<<<< HEAD
    uint32_t health;
 } player;
 
 void player_init(player players[]);
 void player_update(void *data);
 uint32_t player_get_beat_rate(player players[]);
-=======
-   uint8_t health;
-} player;
-
-void player_init(player players[]);
-void player_update(player *players);
->>>>>>> 80b432be3a54b6755150eed4a48d6ce5090959ac
 
 #endif
