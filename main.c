1f72351887ebeed3bf90b69cf4bcc2b5d765c784
BlankSparrow <blanksparrow@outlook.com>
2016-10-12 22:51:07 +1300

    corrected trivial main errors

--- main.c
+++ main.c
@@ -50,11 +50,11 @@ void run_host(void){
          // update zombies
          // update player
          {.func = player_update,            .period = 1000,     .data = players},
-         {.func = level_update_client,    .period = 1000,     .data = 0},
+         {.func = level_update_client,    .period = 5000,     .data = 0},
          
          // send data to client
 		 // TODO: Make this part of the player_update function
-		 {.func = send_next_message, 		.period = 1000, 		.data = 0},
+		 {.func = send_next_message, 		.period = 100, 		.data = 0},
          // draw game
          {.func = display_pulse,            .period = 800,      .data = 0}, // drawing a test pattern
          {.func = display_set_camera,       .period = 200,      .data = players},
@@ -83,7 +83,7 @@ void run_client(void){
          // update zombies
          // update player
          {.func = player_update,            .period = 1000,     .data = players},
-         {.func = send_next_message, 		.period = 1000, 		.data = 0},
+         {.func = send_next_message, 		.period = 200, 		.data = 0},
          // send data to client
          // draw game
          {.func = display_pulse,            .period = 800,      .data = 0}, // drawing a test pattern
